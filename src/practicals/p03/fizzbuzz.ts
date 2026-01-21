c7b373a5f1bf3edffb2394b88956631ea0931ced
chphetsr <68010233@kmitl.ac.th>
2026-01-21 16:05:27 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,22 @@
 // Write your code below
+let input = process.argv[2];
+
+if (Number.isNaN(input) || input <= 0) {
+    process.exit();
+
+}
+
+for (let i = 1; i <= input; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+
+    } else if (!(i % 3 === 0 || i % 5 === 0)) {
+        console.log(`${i}`);
+    }
+}
