5deef17d9a6b128af3a1d9882bd0cc306bc48e3b
chphetsr <68010233@kmitl.ac.th>
2026-01-21 15:47:35 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,25 @@
 // Write your code below
+const point = Number(process.argv[2]);
+
+if (Number.isNaN(point) || point < 0 || point > 100) {
+    
+    console.log("Invalid input");
+
+} else if (point >= 80) {
+    console.log("A");
+
+} else if (point >= 70) {
+    console.log("B");
+
+} else if (point >= 60) {
+    console.log("C");
+
+} else if (point >= 50) {
+    console.log("D");
+
+} else if (point >= 0) {
+    console.log("F");
+
+} else {
+    console.log("Invalid input");
+}
