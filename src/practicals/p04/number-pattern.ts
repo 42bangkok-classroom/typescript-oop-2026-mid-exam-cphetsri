7807fee77092bb81d643695b41c2b0ab765b6c78
chphetsr <68010233@kmitl.ac.th>
2026-01-21 16:29:26 +0700

    u

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,13 @@
 // Write your code below
+let input = process.argv[2];
+if (Number.isNaN(input) || input <= 0){
+    process.exit();
+
+}
+let out = '';
+for (let i = input; i > 0; i++){
+    for (let j = i; j > 0; j--){
+        out += j
+    }
+    out += '\n'
+} console.log(out);
